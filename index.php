b0e6ce76e66afe71d0ca313427e405b05a701c6f
Lech Migdal <lech.migdal@hpe.com>
2016-10-28 15:19:46 +0200

    Removing hard codes

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-$link = mysql_connect('us-cdbr-azure-east-c.cloudapp.net', 'b61c3f898e83cc', '1d6dbb35');
+
+$username = getenv('MYSQLCONNSTR_username');
+$password = getenv('MYSQLCONNSTR_password');
+$connectionString = getenv('MYSQLCONNSTR_string');
+
+$link = mysql_connect($connectionString, $username, $password);
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
 echo 'Connected successfully';
 mysql_close($link);
-
-$connstr = getenv('MYSQLCONNSTR_username');
-echo "Username: $connstr";
 ?>
