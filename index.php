0d6e82bae8dbc897e2d81115cbaa33136f689297
Lech Migdal <lech.migdal@hpe.com>
2016-11-21 12:17:14 +0100

    tt

--- index.php
+++ index.php
@@ -17,10 +17,10 @@ try {
 		));
 
     echo ("Succes in connecting");
-/*
+
 		// sets message to contian "Hello world"
 		$redis->set('message', 'Hello world');
-
+/*
 		// gets the value of message
 		$value = $redis->get('message');
 
